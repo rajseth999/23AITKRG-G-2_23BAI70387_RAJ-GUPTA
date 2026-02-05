65baaf5342d612950901a868f96b3e472f9345d3
rajseth999 <rajseth958958@gmail.com>
2026-02-05 10:00:29 +0530

    EXP3

---
+++ EXP3/eco_trap3/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = ({title}) => {
+    return (
+        <header style={{backgroundColor: "#f0ff67ff", height: 'auto', 
+        borderRadius: 10, paddingLeft: 10}}>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default Header;
